6c3300b34fdebfad6172708c96ca57759ddb69ce
mmoharem <cloud@bnfstore.com>
2018-04-26 23:03:01 +0200

    continue migration

---
+++ database/migrations/2018_04_26_204946_add_is_admin_column_to_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsAdminColumnToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->integer('is_admin')->unsigned(); //unsigned mean that it cannot be a -ve number
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->dropColumn('is_admin');
+        });
+    }
+}
