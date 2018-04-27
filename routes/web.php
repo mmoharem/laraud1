8ff30c3c2b76078db20129d9f6c09c198e6020f2
mmoharem <cloud@bnfstore.com>
2018-04-27 19:00:18 +0200

    finished raw-sql

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,49 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::resource('posts', 'PostsController');
+// Route::get('/contact', 'PostsController@contact');
+
+// Route::get('post/{id}/{name}/{pass}', 'PostsController@show_post');
+
+
+/*
+|--------------------------------------------------------------------------
+| Raw DB SQL Query
+|--------------------------------------------------------------------------
+*/
+
+// Route::get('/insert', function(){
+
+//     //using static method from the database class
+
+//     DB::insert('insert into posts(title, body) values(?, ?)', ['PHP with Laravel', 'Laravel is the best thing happend to PHP']);
+
+// });
+
+
+// Route::get('/read', function(){
+
+//     $results = DB::select('select * from posts where id = ?', [1]);
+
+//     foreach($results as $result){
+//         return $result->title;
+//     }
+
+// });
+
+// Route::get('/update', function(){
+
+//     $updated = DB::update('update posts set title ="Updated title" where id = ?', [1]);
+//     return $updated;
+
+// });
+
+Route::get('/delete', function() {
+    $deleted = DB::delete('delete from posts where id = ?', [2]);
+    return $deleted;
 });
-
-Route::resource('posts', 'PostsController');
-Route::get('/contact', 'PostsController@contact');
-
-Route::get('post/{id}/{name}/{pass}', 'PostsController@show_post');
